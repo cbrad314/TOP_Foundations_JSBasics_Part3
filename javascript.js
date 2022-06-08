2ae929595eaf86716bb5355f6d3d87973d50b849
Chris <chris.p.bradford@gmail.com>
2022-06-08 14:57:37 -0500

    complete TOP Foundations JS Basics Part 3 Practice

---
+++ javascript.js
@@ -0,0 +1,15 @@
+function add7(addend){
+    return(addend + 7);
+}
+
+function multiply (operand1,operand2){
+    return(operand1*operand2);
+}
+
+function capitalize (capitalizeInput) { 
+    return capitalizeInput.charAt(0).toUpperCase()+capitalizeInput.substring(1).toLowerCase();
+}
+
+function lastLetter (lastLetterInput){
+    return(lastLetterInput.substring(lastLetterInput.length-1));
+}
